2e0e1fc19ff654221b955c75c348d2374e4cb4b9
Jasmine Alvarez <jxa709@hpc-compute-node01pa.jefferson.edu>
2024-01-29 17:00:39 -0500

    Add chr len dict

--- with_mut_name.3DTumorSimulMetastasis.py
+++ with_mut_name.3DTumorSimulMetastasis.py
@@ -633,6 +633,31 @@ mut_id = 0
 mutlineage = ['0']                  # the lineage tracer
 ######################################################################################
 # New step - init dict that will map mut_id keys to mutation names
+chr_dict = dict{
+        'chr1': 248956422,
+        'chr2': 242193529,
+        'chr3': 198295559,
+        'chr4': 190214555,
+        'chr5': 181538159,
+        'chr6': 170805979,
+        'chr7': 159345973,
+        'chr8': 145138636,
+        'chr9': 138394717,
+        'chr10': 133797422,
+        'chr11': 135086622,
+        'chr12': 133275309,
+        'chr13': 114364328,
+        'chr14': 107043718,
+        'chr15': 101991189,
+        'chr16': 90338345,
+        'chr17': 83257441,
+        'chr18': 80373285,
+        'chr19': 58617616,
+        'chr20': 64444167,
+        'chr21': 46709983,
+        'chr22': 50818468,
+        'chrX': 156040895
+        }
 mut_dict = dict()
 mut_dict[str(mut_id)] = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
 
